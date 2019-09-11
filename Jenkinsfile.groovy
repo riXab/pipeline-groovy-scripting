440457d50c75e91257d68ea4fd6961be8daf2c1c
Rishab Shah <Rishab@Shah>
2019-09-12 04:32:25 +0530

    Commit project - In Groovy file via pipeline - 13

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,17 @@
-import hudson.model.*
-import hudson.plugins.*
-import java.util.concurrent.CancellationException
-
-mavenJob('my-mvn-job') {
-    logRotator(-1, 10)
-    jdk('localJDK')
-    scm {
-        github('riXab/pipeline-groovy-scripting', 'master')
-    }
-    triggers {
-      pollSCM{
-      	scmpoll_spec('* * * * *')
-      }
-    }
-    goals('clean package')
-    publishers {
-        archiveArtifacts('**/*.war')
-		
-    }
+import hudson.util.RemotingDiagnostics
+import jenkins.model.Jenkins
+ 
+String agent_name = 'your agent name'
+//groovy script you want executed on an agent
+groovy_script = '''
+println System.getenv("PATH")
+println "uname -a".execute().text
+'''.trim()
+ 
+String result
+Jenkins.instance.slaves.find { agent ->
+    agent.name == agent_name
+}.with { agent ->
+    result = RemotingDiagnostics.executeGroovy(groovy_script, agent.channel)
 }
+println result
