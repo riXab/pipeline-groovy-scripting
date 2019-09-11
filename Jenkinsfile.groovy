60a3fc553ad48ffea40382597e6caa963f9e5d5d
Rishab Shah <Rishab@Shah>
2019-09-12 05:09:02 +0530

    Commit project - In Groovy file via pipeline - 14

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,6 +4,4 @@ println "Print Trial"
 println 'Print Trial Again'
 println System.getenv("PATH")
 println "Print Trial And Again"
-println "uname -a".execute().text
-'''.trim()
- 
+println "uname -a".execute().text 
