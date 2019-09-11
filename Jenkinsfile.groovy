75278b4f3872a3d7b73d1923933aa4cdb98a1ca9
Rishab Shah <Rishab@Shah>
2019-09-12 05:19:17 +0530

    Commit project - In Groovy file via pipeline - 15

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,19 @@
+//Trial 1
 String agent_name = 'your agent name'
 println agent_name
 println "Print Trial"
 println 'Print Trial Again'
-println System.getenv("PATH")
+println System.getenv("PATH")     // Note: This line not working when this script is called via 'Pipeline' as there is no BUILD section to select 'Execute Groovy Script'
 println "Print Trial And Again"
-println System.getenv("JAVA_HOME")
+println System.getenv("JAVA_HOME") // Note: This line not working when this script is called via 'Pipeline' as there is no BUILD section to select 'Execute Groovy Script'
+
+//Trial 2
+def example1() {
+  println 'Hello from example1'
+}
+
+def example2() {
+  println 'Hello from example2'
+}
+
+return this
