f38bb06196ecc90a290c6d4fee64195a652a4743
Rishab Shah <Rishab@Shah>
2019-09-12 07:24:15 +0530

    Commit project - In Groovy file via pipeline - 19

--- Jenkin.groovy
+++ Jenkin.groovy
@@ -5,5 +5,5 @@ println 'Print Trial Again'
 println System.getenv("PATH")     // Note: This line not working when this script is called via 'Pipeline' as there is no BUILD section to select 'Execute Groovy Script'
 println "Print Trial And Again"
 println System.getenv("JAVA_HOME") // Note: This line not working when this script is called via 'Pipeline' as there is no BUILD section to select 'Execute Groovy Script'
-System.getenv('GITHUB_USERNAME')
-System.getenv('GITHUB_PASSWORD')
+println System.getenv('GITHUB_USERNAME')
+println System.getenv('GITHUB_PASSWORD')
