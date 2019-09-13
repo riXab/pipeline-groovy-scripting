3e6b4861fdfa6805db8a6d5141026fefe11b5070
Rishab Shah <Rishab@Shah>
2019-09-13 05:51:44 +0530

    Again Edited and Again

--- Jenfile.groovy
+++ Jenfile.groovy
@@ -3,7 +3,7 @@ def example1() {
   println 'Starting 1st job'
 			//logRotator(-1, 10)
 			git url: "https://github.com/riXab/groovy-pipeline-scripting.git"
-			def mvnHome = tool 'M3'
+			def mvnHome = tool 'localMaven'
 			bat "${mvnHome}\\bin\\mvn -B verify"
 		}
 
