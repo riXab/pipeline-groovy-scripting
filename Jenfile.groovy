449d5788220046833244f5487ccf3fffff14511b
Rishab Shah <Rishab@Shah>
2019-09-13 08:01:52 +0530

    Again Edited

--- Jenfile.groovy
+++ Jenfile.groovy
@@ -3,12 +3,16 @@ def example1() {
   println 'Starting 1st job'
 			//logRotator(-1, 10)
 			git url: "https://github.com/riXab/groovy-pipeline-scripting.git"
-		//	jdk(localJDK)
+		
 			def jdk = tool name: 'localJDK'
 			env.JAVA_HOME = "${jdk}"
-			//bat "${jdk}\\bin\\java -version"
+			
+			//def mvnHome = tool 'localMaven'
+			//bat "${mvnHome}\\bin\\mvn -B verify"
+			//OR:
 			def mvnHome = tool 'localMaven'
-			bat "${mvnHome}\\bin\\mvn -B verify"
+			env.PATH = "${mvnHome}/bin:${env.PATH}"
+			bat 'mvn -B verify'
 		}
 
 def example2() {
