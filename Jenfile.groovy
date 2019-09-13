811b38e11b17ea636839e904445b1a7e579bc8da
Rishab Shah <Rishab@Shah>
2019-09-13 08:21:48 +0530

    Again Edited

--- Jenfile.groovy
+++ Jenfile.groovy
@@ -8,12 +8,13 @@ def example1() {
 			env.JAVA_HOME = "${jdk}"
 			
 			//def mvnHome = tool 'localMaven'
-			//bat "${mvnHome}\\bin\\mvn -B verify"
+			//bat "${mvnHome}\\bin\\mvn -B -Dmaven.test.failure.ignore verify"
 			//OR:
 			withEnv(["PATH+MAVEN=${tool 'localMaven'}/bin"]) {
-			bat 'mvn -B verify'
-  }
-			
+				bat 'mvn -B verify'
+			}
+			step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
+			step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 		}
 
 def example2() {
